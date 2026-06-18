9aa219dd7f06f0eeeba7f56b2b6239a7316ce622
Timur <154419832+Timur10121@users.noreply.github.com>
2026-06-18 18:07:27 +0000

    sizzee

--- botolabr_new/frontend/src/views/LandingView.vue
+++ botolabr_new/frontend/src/views/LandingView.vue
@@ -722,8 +722,8 @@ const faqItems = [
 }
 
 .variant__content-image {
-  width: clamp(450px, 50vw, 600px);
-  height: clamp(450px, 50vw, 600px);
+  width: clamp(300px, 50vw, 450px);
+  height: clamp(300px, 50vw, 450px);
   background: #F6FEDE;
   background-size: cover;
   background-position: center;
